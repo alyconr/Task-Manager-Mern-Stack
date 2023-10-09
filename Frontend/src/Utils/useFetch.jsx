5e856f572fb1faac82f6a327ce39e6b42168dcec
alyconr <alyconr@hotmail.com>
2023-10-09 15:46:33 -0500

    :fire: Update custom  use fetch

--- Frontend/src/Utils/useFetch.jsx
+++ Frontend/src/Utils/useFetch.jsx
@@ -6,26 +6,24 @@ const useFetch = (url) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  
   useEffect(() => {
-   setTimeout(() => {
+    setTimeout(() => {
       axios
-      .get(url)
-      .then((response) => {
-        if (response.status !== 200) {
-          throw new Error("Could not fetch the data for that resource");
-        }
-        setData(response.data);
-        setLoading(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setLoading(false);
-        setData(null);
-      });
-   } , 500); 
-   
+        .get(url)
+        .then((response) => {
+          if (response.status !== 200) {
+            throw new Error("Could not fetch the data for that resource");
+          }
+          setData(response.data);
+          setLoading(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setLoading(false);
+          setData(null);
+        });
+    }, 500);
   }, [url]);
 
   return { data, loading, error };
